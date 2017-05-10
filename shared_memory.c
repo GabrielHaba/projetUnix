ee3fd41b5a7b2a77804214e72341c63aa1b79f8c
GabrielHaba <gabriel.haba@gmail.com>
2017-05-10 08:58:33 +0200

    Début de la création des modules pour la mémoire partagée

---
+++ shared_memory.c
@@ -0,0 +1,36 @@
+/****************************************
+ *  Fichier shared_memory.c
+ *  Projet UNIX
+ *  Serie 1
+ *                          Haba, Gabriel
+ *                    Nuez Soriano, Diego
+ ****************************************/
+
+#include "shared_memory.h"
+
+int creerSharedM(){
+  int shmid=-1;
+  if((shmid=shmget(key,SHMSIZE,SHMFLAG))<0){
+    perror("Erreur lors de la création de la mémoire partagée ! ");
+    exit(6);
+  }
+  return shmid;
+}
+
+void *attacherSharedM(int shmid){
+  void *addr=NULL;
+
+  if ((addr=shmat(shmid,NULL,0)<0){
+    perror("Erreur lors du rattachement du segment à l'espace de données du processus");
+    exit(6);
+  }
+
+  return addr;
+}
+
+void detacherSharedM(void *addr){
+  if(shmdt(addr)<0){
+    perror("Erreur lors du détachement du segment de mémoire partagée !");
+    exit(6);
+  }
+}
---
+++ shared_memory.h
@@ -0,0 +1,42 @@
+/****************************************
+ *	Fichier d'entete de shared_memory.h
+ *	Projet UNIX
+ *  Serie 1
+ *							Haba, Gabriel
+ *					  Nuez Soriano, Diego
+ ****************************************/
+
+#include <sys/types.h>
+#include <sys/shm.h>
+#include <sys/ipc.h>
+#include <stdlib.h>
+
+#ifndef _SHARED_H_
+
+	#define _SHARED_H_
+
+	key_t key = 9;
+
+ 	#define SHMSIZE = 1024;
+
+	#define SHMFLAG = IPC_CREATE;
+
+	typedef struct record{
+		char label[64];
+		void *contenu;
+	} Record;
+
+	int creerSharedM();
+
+	void * attacherSharedM(int);
+
+	void detacherSharedM(void *);
+
+	void supprimerSharedMemory(int);
+
+	void lireSharedM(int);
+
+	void ecrireSharedM(int);
+
+
+#endif
